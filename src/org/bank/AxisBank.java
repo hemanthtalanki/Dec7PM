41a8d859335a6fc6fb18fbd1fb3821ccf45ff077
hemanthtalanki <hemanthtalanki.44@gmail.com>
2021-12-05 15:34:30 +0530

    task

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,21 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+	private void deposit() {
+		System.out.println("custmer deposit amount is 1500");
+		
+	
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		AxisBank a = new AxisBank();
+		a.deposit();
+		a.saving();
+		a.fixed();
+		
+		
+		
+	}
+
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,15 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("saving account is 74347303840");
+		
+		
+	}
+	public void fixed() {
+		System.out.println("custmer fixed account is 5986968787");
+		// TODO Auto-generated method stub
+
+	}
+
+}
